60a945568133354950e6866f9dcd44025aae7f21
Robin Harms Oredsson <robin@betahaus.net>
2017-10-06 16:09:22 +0200

    Testing

--- arche_pas/providers/oauth2.py
+++ arche_pas/providers/oauth2.py
@@ -11,27 +11,40 @@ class OAuth2Generic(PASProvider):
     title = _("OAuth2")
     id_key = 'id'
     paster_config_ns = __name__
-    default_settings = {}
-
-    def get_session(self):
-        return OAuth2Session(self.settings['client_id'],
-                             #scope=self.settings['scope'],
-                             redirect_uri=self.callback_url())
+    default_settings = {
+    #    "access_type": "offline",
+    }
 
     def begin(self):
-        auth_session = self.get_session()
-        authorization_url, state = auth_session.authorization_url(self.settings['auth_uri'],
-                                                            #access_type=self.settings['access_type'],
-                                                            #approval_prompt=self.settings['approval_prompt']
+        auth_session = OAuth2Session(
+            client_id=self.settings['client_id'],
+            #scope=self.settings['scope'],
+            response_type='code',
+            redirect_uri=self.callback_url()
+        )
+        authorization_url, state = auth_session.authorization_url(
+            self.settings['auth_uri'],
+            #access_type=self.settings['access_type'],
+            #approval_prompt=self.settings['approval_prompt']
         )
         return authorization_url
 
     def callback(self):
-        auth_session = self.get_session()
+        auth_session = OAuth2Session(
+            client_id=self.settings['client_id'],
+            #scope=self.settings['scope'],
+            response_type='code',
+            redirect_uri=self.callback_url()
+        )
         #Should we do anything with the token response? Auth is handled by Arche anyway.
-        res = auth_session.fetch_token(self.settings['token_uri'],
-                                client_secret=self.settings['client_secret'],
-                                authorization_response=self.request.url)
+        res = auth_session.fetch_token(
+            self.settings['token_uri'],
+            code=self.request.GET.get('code', ''),
+            client_secret=self.settings['client_secret'],
+                                #authorization_response=self.request.url
+       )
+
+
         profile_response = auth_session.get(self.settings['profile_uri'])
         profile_data = profile_response.json()
         return profile_data
