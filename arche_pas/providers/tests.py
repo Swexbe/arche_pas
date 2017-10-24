58ba8c8a822e70bf58826aafe3ac918394e4dce2
Robin Harms Oredsson <robin@betahaus.net>
2017-10-24 13:58:41 +0200

    Will probably never be implemented

--- arche_pas/providers/tests.py
+++ arche_pas/providers/tests.py
@@ -47,29 +47,9 @@ class GoogleOAuth2Tests(unittest.TestCase):
 
     def test_verify_class(self):
         self.failUnless(verifyClass(IPASProvider, self._cut))
-#
-#
-# class OpenIDAuthTests(unittest.TestCase):
-#     def setUp(self):
-#         self.config = testing.setUp()
-#
-#     def tearDown(self):
-#         testing.tearDown()
-#
-#     @property
-#     def _cut(self):
-#         from arche_pas.providers.openid import OpenIDAuth
-#         return OpenIDAuth
-#
-#     def test_verify_object(self):
-#         context = testing.DummyResource()
-#         request = testing.DummyRequest()
-#         self.failUnless(verifyObject(IPluggableAuth, self._cut(context, request)))
-#
-#     def test_verify_class(self):
-#         self.failUnless(verifyClass(IPluggableAuth, self._cut))
-#
-#
+
+
+
 # class TwitterAuthTests(unittest.TestCase):
 #     def setUp(self):
 #         self.config = testing.setUp()
