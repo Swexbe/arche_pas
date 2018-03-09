70d4679e6f314c6684ba84ed616562ba5f112365
Robin Harms Oredsson <robin@betahaus.net>
2018-03-09 09:26:32 +0100

    Translation

--- update_translations.sh
+++
@@ -1,8 +0,0 @@
- #!/bin/bash
- #You need lingua and gettext installed to run this
- 
- echo "Updating arche_pas.pot"
- pot-create -d arche_pas -o arche_pas/locale/arche_pas.pot arche_pas/.
- echo "Merging Swedish localisation"
- msgmerge --update  arche_pas/locale/sv/LC_MESSAGES/arche_pas.po  arche_pas/locale/arche_pas.pot
- echo "Updated locale files"
