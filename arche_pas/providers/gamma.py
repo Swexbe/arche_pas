ca398083762c7950dc5a342e16a259d10eb7198c
Swexbe <erik@erikjohnsson.se>
2020-05-07 18:19:57 +0200

    Test fail

--- arche_pas/providers/gamma.py
+++ arche_pas/providers/gamma.py
@@ -43,6 +43,7 @@ class GammaOAuth2(PASProvider):
         return profile_data
 
     def get_email(self, response, validated=False):
+        raise Exception("hello")
         return response.get('email', None)
 
     def get_profile_image(self, response):
